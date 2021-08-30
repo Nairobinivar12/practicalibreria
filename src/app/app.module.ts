a0207725454f64097c16d2db717df7c998f2c515
Nairobinivar12 <ingenieranairobi@gmail.com>
2021-08-30 20:42:18 +0200

    subiendo el proyecto de la libreria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { EscritoresComponent } from './components/escritores/escritores.component';
+import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
+import { PromocionComponent } from './components/promocion/promocion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent,
+    EscritoresComponent,
+    TarjetasComponent,
+    PromocionComponent
   ],
   imports: [
     BrowserModule,
